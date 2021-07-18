2fe21984442ae9daf4ef2e045c0982efc80349ac
Andrew Wang <ay.wang@mail.utoronto.ca>
2021-07-18 17:55:53 -0400

    ui wip

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,35 +1,60 @@
 <template>
     <el-container class="h-full" direction="vertical">
         <el-header class="bg-purple-100"> Seam Carving </el-header>
-        <el-header class="bg-purple-100">
-            <el-input class="w-80" plaholder="Url" v-model="url"></el-input>
-            <el-button @click="loadNew">load</el-button>
-            <el-button @click="something">do something</el-button>
-        </el-header>
         <el-main class="bg-red-100 text-center">
-            <canvas class="img-canvas m-auto"></canvas>
+            <div class="bg-blue-100 flex">
+                <el-form :model="sizeForm">
+                    <el-form-item label="Width" label-width="auto">
+                        <el-input class="w-40 mx-10" v-model="sizeForm.width" type="number">
+                            <template #append>{{ unitText }}</template>
+                        </el-input>
+                    </el-form-item>
+                    <el-form-item label="Height" label-width="auto">
+                        <el-input class="w-40 mx-10" v-model="sizeForm.height" type="number">
+                            <template #append>{{ unitText }}</template>
+                        </el-input>
+                    </el-form-item>
+                    <el-switch v-model="pixels" active-text="px" inactive-text="%" inactive-color="#409EFF"></el-switch>
+                </el-form>
+
+                <el-input class="w-80" plaholder="Url" v-model="url"></el-input>
+                <el-button @click="loadNew">load</el-button>
+                <el-button @click="something">do something</el-button>
+            </div>
+            <canvas class="img-canvas m-auto max-w-full max-h-full"></canvas>
         </el-main>
         <el-footer class="bg-green-100"> feet </el-footer>
     </el-container>
 </template>
 
 <script setup>
-import { defineProps, ref, onMounted } from 'vue';
+import { computed, ref, onMounted } from 'vue';
 import Carver from '@/carver/carver';
 
 let canvas;
 let ctx;
 let carver;
-let url = ref('/src/assets/images/chameleon.png');
+let url = ref('');
+let pixels = ref(true);
+
+let unitText = computed(() => (pixels.value ? 'px' : '%'));
+let widthInput = ref(0);
+let heightInput = ref(0);
+
+let sizeForm = ref({
+    width: 0,
+    height: 0,
+});
 
 onMounted(() => {
     canvas = document.querySelector('.img-canvas');
     ctx = canvas.getContext('2d');
+    loadNew('/src/assets/images/chameleon.png');
 });
 
-const loadNew = () => {
+const loadNew = (url) => {
     let img = new Image();
-    img.src = url.value;
+    img.src = url;
     img.crossOrigin = 'Anonymous';
     img.onload = () => {
         canvas.width = img.width;
@@ -52,9 +77,9 @@ const something = () => {
             canvas.width = imageData.width;
             canvas.height = imageData.height;
             ctx.putImageData(imageData, 0, 0);
-        }, 150);
+        }, 50);
         if (++i === 100) clearInterval(interval);
-    }, 200);
+    }, 100);
 };
 </script>
 
